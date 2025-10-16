313ce2b95739aad7e7e349791d7f8a4e427018ad
masharzya1 <masharzya1@gmail.com>
2025-10-16 07:07:22 +0600

    update

--- server.js
+++ server.js
@@ -56,6 +56,4 @@ Generate the most suitable message according to this situation and the specified
     }
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
+
