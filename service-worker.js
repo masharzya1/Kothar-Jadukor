e99395d00e439ef41673803844617c1f281953a2
masharzya1 <masharzya1@gmail.com>
2025-10-16 14:41:04 +0600

    update

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+
+
+const CACHE_NAME = 'kothar-jadukor-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
